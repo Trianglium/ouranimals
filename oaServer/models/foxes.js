984c765d0452008dae1e7a45992407fbfb9ee3ba
Trianglium <jbrockerrudow@gmail.com>
2022-03-30 00:14:49 -0500

    Fox and Otter Schema finsihed

---
+++ oaServer/models/foxes.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const foxSchema = new Schema({
+    image: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+
+
+var Foxes = mongoose.model('Fox', foxSchema);
+module.exports = Foxes;
