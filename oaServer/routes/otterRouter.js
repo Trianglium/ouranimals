4608799aa23e01e6f70b19e371bfed52c590a3dc
Trianglium <jbrockerrudow@gmail.com>
2022-03-30 00:21:18 -0500

    Otters REST API Endpoints

--- oaServer/routes/otterRouter.js
+++ oaServer/routes/otterRouter.js
@@ -1,55 +1,81 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Integrate otter Schema
+const Otters = require('../models/otters');
 
 const otterRouter = express.Router();
 
 otterRouter.use(bodyParser.json());
 
-// otters
 otterRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req,res,next) => {
-    res.end('Will send all the otters to you!');
+    Otters.find({})
+    .then((otters) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(otters);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req, res, next) => {
-    res.end('Will add the otter: ' + req.body.name + ' with details: ' + req.body.description);
+    Otters.create(req.body)
+    .then((otter) => {
+        console.log('otter Created ', otter);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(otter);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .put((req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /otters');
 })
 .delete((req, res, next) => {
-    res.end('Deleting all otters');
+    Otters.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
-
-// otterId
 otterRouter.route('/:otterId')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req,res,next) => {
-    res.end('Will send details of the otter: ' + req.params.otterId +' to you!');
+    Otters.findById(req.params.otterId)
+    .then((otter) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(otter);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
-
 .post((req, res, next) => {
-  res.statusCode = 403;
-  res.end('POST operation not supported on /otters/'+ req.params.otterId);
+    res.statusCode = 403;
+    res.end('POST operation not supported on /otters/'+ req.params.otterId);
 })
-
 .put((req, res, next) => {
-  res.write('Updating the otter: ' + req.params.otterId + '\n');
-  res.end('Will update the otter: ' + req.body.name + ' with details: ' + req.body.description);
+    Otters.findByIdAndUpdate(req.params.otterId, {
+        $set: req.body
+    }, { new: true })
+    .then((otter) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(otter);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 })
-
 .delete((req, res, next) => {
-    res.end('Deleting otter: ' + req.params.otterId);
+    otters.findByIdAndRemove(req.params.otterId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
 module.exports = otterRouter;
