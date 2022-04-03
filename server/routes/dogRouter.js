7dae546e074ed2dca857fb6d5d366b55096d3a87
Trianglium <jbrockerrudow@gmail.com>
2022-04-03 13:22:33 -0500

    Add support for acknowledgements

--- server/routes/dogRouter.js
+++ server/routes/dogRouter.js
@@ -3,13 +3,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const Dogs = require('../models/dogs');
-
 const dogRouter = express.Router();
 
 dogRouter.use(bodyParser.json());
 
-dogRouter.use(bodyParser.json());
 
+// All Dogs
 dogRouter.route('/')
 .get((req,res,next) => {
     Dogs.find({})
@@ -44,6 +43,8 @@ dogRouter.route('/')
     .catch((err) => next(err));
 });
 
+
+// Dog specified by ID
 dogRouter.route('/:dogId')
 .get((req,res,next) => {
     Dogs.findById(req.params.dogId)
@@ -75,6 +76,157 @@ dogRouter.route('/:dogId')
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+// All Acknowledgements
+dogRouter.route('/:dogId/acknowledgements')
+.get((req,res,next) => {
+    Dogs.findById(req.params.dogId)
+    .then((dog) => {
+        if (dog != null) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(dog.acknowledgements);
+        }
+        else {
+            err = new Error('Dog ' + req.params.dogId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+    Dogs.findById(req.params.dogId)
+    .then((dog) => {
+        if (dog != null) {
+            dog.acknowledgements.push(req.body);
+            dog.save()
+            .then((dog) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dog);
+            }, (err) => next(err));
+        }
+        else {
+            err = new Error('Dog ' + req.params.dogId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dogs/'
+        + req.params.dogId + '/acknowledgements');
+})
+.delete((req, res, next) => {
+    Dogs.findById(req.params.dogId)
+    .then((dog) => {
+        if (dog != null) {
+            for (var i = (dog.acknowledgements.length -1); i >= 0; i--) {
+                dog.acknowledgements.id(dog.acknowledgements[i]._id).remove();
+            }
+            dog.save()
+            .then((dog) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dog);
+            }, (err) => next(err));
+        }
+        else {
+            err = new Error('Dog ' + req.params.dogId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+// Acknowledgement specified by ID
+
+dogRouter.route('/:dogId/acknowledgements/:acknowledgementId')
+.get((req,res,next) => {
+    Dogs.findById(req.params.dogId)
+    .then((dog) => {
+        if (dog != null && dog.acknowledgements.id(req.params.acknowledgementId) != null) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(dog.acknowledgements.id(req.params.acknowledgementId));
+        }
+        else if (dog == null) {
+            err = new Error('Dog ' + req.params.dogId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+        else {
+            err = new Error('Acknowledgement ' + req.params.acknowledgementId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dogs/'+ req.params.dogId
+        + '/acknowledgements/' + req.params.acknowledgementId);
+})
+.put((req, res, next) => {
+    Dogs.findById(req.params.dogId)
+    .then((dog) => {
+        if (dog != null && dog.acknowledgements.id(req.params.acknowledgementId) != null) {
+            if (req.body.rating) {
+                dog.acknowledgements.id(req.params.acknowledgementId).rating = req.body.rating;
+            }
+            if (req.body.acknowledgement) {
+                dog.acknowledgements.id(req.params.acknowledgementId).acknowledgement = req.body.acknowledgement;
+            }
+            dog.save()
+            .then((dog) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dog);
+            }, (err) => next(err));
+        }
+        else if (dog == null) {
+            err = new Error('Dog ' + req.params.dogId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+        else {
+            err = new Error('Acknowledgement ' + req.params.acknowledgementId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.delete((req, res, next) => {
+    Dogs.findById(req.params.dogId)
+    .then((dog) => {
+        if (dog != null && dog.acknowledgements.id(req.params.acknowledgementId) != null) {
+            dog.acknowledgements.id(req.params.acknowledgementId).remove();
+            dog.save()
+            .then((dog) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(dog);
+            }, (err) => next(err));
+        }
+        else if (dog == null) {
+            err = new Error('Dog ' + req.params.dogId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+        else {
+            err = new Error('Acknowledgement ' + req.params.acknowledgementId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
     }, (err) => next(err))
     .catch((err) => next(err));
 });
--- server/routes/foxRouter.js
+++ server/routes/foxRouter.js
@@ -3,13 +3,11 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const Foxes = require('../models/foxes');
-
 const foxRouter = express.Router();
 
 foxRouter.use(bodyParser.json());
 
-foxRouter.use(bodyParser.json());
-
+// All Foxes
 foxRouter.route('/')
 .get((req,res,next) => {
     Foxes.find({})
@@ -44,6 +42,8 @@ foxRouter.route('/')
     .catch((err) => next(err));
 });
 
+
+// Fox specified by ID
 foxRouter.route('/:foxId')
 .get((req,res,next) => {
     Foxes.findById(req.params.foxId)
@@ -75,6 +75,157 @@ foxRouter.route('/:foxId')
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+// All Acknowledgements
+foxRouter.route('/:foxId/acknowledgements')
+.get((req,res,next) => {
+    Foxes.findById(req.params.foxId)
+    .then((fox) => {
+        if (fox != null) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(fox.acknowledgements);
+        }
+        else {
+            err = new Error('Fox ' + req.params.foxId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+    Foxes.findById(req.params.foxId)
+    .then((fox) => {
+        if (fox != null) {
+            fox.acknowledgements.push(req.body);
+            fox.save()
+            .then((fox) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(fox);
+            }, (err) => next(err));
+        }
+        else {
+            err = new Error('Fox ' + req.params.foxId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /foxes/'
+        + req.params.foxId + '/acknowledgements');
+})
+.delete((req, res, next) => {
+    Foxes.findById(req.params.foxId)
+    .then((fox) => {
+        if (fox != null) {
+            for (var i = (fox.acknowledgements.length -1); i >= 0; i--) {
+                fox.acknowledgements.id(fox.acknowledgements[i]._id).remove();
+            }
+            fox.save()
+            .then((fox) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(fox);
+            }, (err) => next(err));
+        }
+        else {
+            err = new Error('Fox ' + req.params.foxId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+// Acknowledgement specified by ID
+
+foxRouter.route('/:foxId/acknowledgements/:acknowledgementId')
+.get((req,res,next) => {
+    Foxes.findById(req.params.foxId)
+    .then((fox) => {
+        if (fox != null && fox.acknowledgements.id(req.params.acknowledgementId) != null) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(fox.acknowledgements.id(req.params.acknowledgementId));
+        }
+        else if (fox == null) {
+            err = new Error('Fox ' + req.params.foxId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+        else {
+            err = new Error('Acknowledgement ' + req.params.acknowledgementId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /foxes/'+ req.params.foxId
+        + '/acknowledgements/' + req.params.acknowledgementId);
+})
+.put((req, res, next) => {
+    Foxes.findById(req.params.foxId)
+    .then((fox) => {
+        if (fox != null && fox.acknowledgements.id(req.params.acknowledgementId) != null) {
+            if (req.body.rating) {
+                fox.acknowledgements.id(req.params.acknowledgementId).rating = req.body.rating;
+            }
+            if (req.body.acknowledgement) {
+                fox.acknowledgements.id(req.params.acknowledgementId).acknowledgement = req.body.acknowledgement;
+            }
+            fox.save()
+            .then((fox) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(fox);
+            }, (err) => next(err));
+        }
+        else if (fox == null) {
+            err = new Error('Fox ' + req.params.foxId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+        else {
+            err = new Error('Acknowledgement ' + req.params.acknowledgementId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.delete((req, res, next) => {
+    Foxes.findById(req.params.foxId)
+    .then((fox) => {
+        if (fox != null && fox.acknowledgements.id(req.params.acknowledgementId) != null) {
+            fox.acknowledgements.id(req.params.acknowledgementId).remove();
+            fox.save()
+            .then((fox) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(fox);
+            }, (err) => next(err));
+        }
+        else if (fox == null) {
+            err = new Error('Fox ' + req.params.foxId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
+        else {
+            err = new Error('Acknowledgement ' + req.params.acknowledgementId + ' not found');
+            err.status = 404;
+            return next(err);
+        }
     }, (err) => next(err))
     .catch((err) => next(err));
 });
