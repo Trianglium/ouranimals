caaefe679dca4a8c9d4e9a3f4363b60e84f75a97
Trianglium <jbrockerrudow@gmail.com>
2022-04-03 12:06:24 -0500

    Integrate Acknowledgements schema into otter model

--- oa-mongoose/models/otters.js
+++ oa-mongoose/models/otters.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+var ownerSchema = new Schema({
+  name: String,
+  contact: String,
+});
+
 const otterSchema = new Schema({
     image: {
         type: String,
         required: true,
         unique: true
-    }
+    },
+    acknowledgements: [ ownerSchema ]
 },{
-    timestamps: true
+    unsplash: "https://unsplash.com/s/photos/otter?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
 });
 
 var Otters = mongoose.model('Otter', otterSchema);
