5a44608fe9e9bf23a44fe0ef2dddbc68ce2d68d4
Trianglium <jbrockerrudow@gmail.com>
2022-04-03 12:06:50 -0500

    ODM Integration into main app

--- oa-mongoose/index.js
+++ oa-mongoose/index.js
@@ -10,17 +10,30 @@ connect.then((db) => {
     console.log('Connected correctly to server');
 
     Otters.create({
-      image: 'static/images/otters/1.jpg'
+      image: 'https://github.com/Trianglium/ouranimals/tree/main/oaServer/public/images/otters'
     })
     .then((otter) => {
         console.log(otter);
 
-        return Otters.find({}).exec();
+        return Otters.findByIdAndUpdate(otter._id, {
+          $set: { image: 'static/images/otters/3.jpg'}
+        }, {
+          new: true
+        }).exec();
+    })
+    .then((otter) => {
+        console.log(otter);
+
+        otter.acknowledgements.push({
+            name: "Nathaniel Yeo",
+            contact: "https://unsplash.com/@nathanielyeo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+        });
+        return otter.save();
     })
     .then((otters) => {
         console.log(otters);
 
-        return Otters.remove({});
+        return Otters.deleteOne({});
     })
     .then(() => {
         return mongoose.connection.close();
@@ -28,5 +41,8 @@ connect.then((db) => {
     .catch((err) => {
         console.log(err);
     });
+
+
+
 
 });
