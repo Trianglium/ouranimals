15f296ed0b5caa394a6b97cb1977ae22eeff8c32
Trianglium <jbrockerrudow@gmail.com>
2022-04-04 07:35:51 -0500

    Update project title

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'OurAnimals' });
+  res.render('index', { title: 'OurAnimals Image Library' });
 });
 
 module.exports = router;
