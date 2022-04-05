e8a4ff3b427e5d07bd8e4c8a1c1f118ce017a6c9
Trianglium <jbrockerrudow@gmail.com>
2022-04-05 02:47:38 -0500

    Placeholder comps for animal endpoints

---
+++ client/src/components/dogComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Media, Button } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
+class DogEndpoint extends Component {
+  render() {
+    return(
+      <div></div>
+    );
+  }
+}
