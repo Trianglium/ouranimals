6d8e0c4a8e7b6b912d73428e7125b043ff78a05a
Trianglium <jbrockerrudow@gmail.com>
2022-04-05 10:05:46 -0500

    Function based components - header

--- client/src/components/headerComponent.js
+++ client/src/components/headerComponent.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import { Media, UncontrolledCarousel, Nav, Navbar, NavbarBrand, Collapse, NavItem, Button } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
-class Header extends Component {
-  render() {
+function Header(props) {
     return(
-      <div>
-        <Navbar dark expand="lg">
+      <div className="header">
+        <Navbar dark>
         <div className="container">
         <NavbarBrand className="mr-auto" href="/">OurAnimals</NavbarBrand>
           <Nav className="ml-auto">
@@ -15,45 +14,13 @@ class Header extends Component {
             <NavLink className="nav-link"  to='/dogs'>Dogs</NavLink>
             <NavLink className="nav-link"  to='/foxes'>Foxes</NavLink>
             <NavLink className="nav-link"  to='/otters'>Otters</NavLink>
-            <NavLink className="nav-link"  to='/'>Documentation</NavLink>
+            <NavLink className="nav-link"  to='/docs'>Documentation</NavLink>
             </NavItem>
           </Nav>
         </div>
         </Navbar>
-
-        <div className="jumbotron">
-                    <div className="container">
-                        <div className="row row-header">
-                            <div className="col-12">
-                              <UncontrolledCarousel
-                                items={[
-                                  {
-                                    altText: 'Slide 1',
-                                    caption: 'Slide 1',
-                                    key: 1,
-                                    src: 'https://picsum.photos/id/123/1200/600'
-                                  },
-                                  {
-                                    altText: 'Slide 2',
-                                    caption: 'Slide 2',
-                                    key: 2,
-                                    src: 'https://picsum.photos/id/456/1200/600'
-                                  },
-                                  {
-                                    altText: 'Slide 3',
-                                    caption: 'Slide 3',
-                                    key: 3,
-                                    src: 'https://picsum.photos/id/678/1200/600'
-                                  }
-                                ]}
-                               />
-                            </div>
-                        </div>
-                    </div>
-                </div>
       </div>
-    );
-  }
+    )
 }
 
 export default Header;
