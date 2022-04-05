d76123dc11a06820b286a974508a59305eccfdaf
Trianglium <jbrockerrudow@gmail.com>
2022-04-05 02:50:39 -0500

    Update image utils to mass print mongo db commands

--- server/public/images/utils/mass_rename.py
+++ server/public/images/utils/mass_rename.py
@@ -1,8 +1,21 @@
 
 import os
-i=1
-for file in os.listdir():
-    src=file
-    dst=str(i)+".jpg"
-    os.rename(src,dst)
-    i+=1
+
+def mass_rename():
+    i=1
+    for file in os.listdir():
+        src=file
+        dst=str(i)+".jpg"
+        os.rename(src,dst)
+        i+=1
+
+
+x = []
+
+for n in range(1, 34):
+    im = f"images/foxes/{str(n)}.jpg"
+    y = {"image": im}
+    x.append(y)
+
+
+print(x)
