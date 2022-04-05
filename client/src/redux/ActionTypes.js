61ceb6b0da7e73ba6f6e89fcc2d2a7244a65871c
Trianglium <jbrockerrudow@gmail.com>
2022-04-05 02:41:22 -0500

    Integrate redux & create action types

---
+++ client/src/redux/ActionTypes.js
@@ -0,0 +1,15 @@
+export const ADD_BEARS = 'ADD_BEARS';
+export const ADD_DOGS = 'ADD_DOGS';
+export const ADD_FOXES = 'ADD_FOXES';
+export const ADD_OTTERS = 'ADD_OTTERS';
+
+
+export const BEARS_FAILED = 'BEARS_FAILED';
+export const DOGS_FAILED = 'DOGS_FAILED';
+export const FOXES_FAILED = 'FOXES_FAILED';
+export const OTTERS_FAILED = 'OTTERS_FAILED';
+
+export const BEARS_LOADING = 'BEARS_LOADING';
+export const DOGS_LOADING = 'DOGS_LOADING';
+export const FOXES_LOADING = 'FOXES_LOADING';
+export const OTTERS_LOADING = 'OTTERS_LOADING';
