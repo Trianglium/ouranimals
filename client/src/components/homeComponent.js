b0d75173a3c986cd22694489b4b652f04eca7186
Trianglium <jbrockerrudow@gmail.com>
2022-04-05 02:48:54 -0500

    Prep container component footer & home comp

---
+++ client/src/components/homeComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Media } from 'reactstrap';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+    return (
+      <>
+        .container-fluid
+        </Container>
+        </>
+    )
+  }
+}
