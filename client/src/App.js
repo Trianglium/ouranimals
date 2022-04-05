8bde66addd6d579746981023436cbcaf2ae0e879
Trianglium <jbrockerrudow@gmail.com>
2022-04-05 03:00:15 -0500

    Temp disintegrate redux

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,18 @@ import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/configureStore';
+//import { Provider } from 'react-redux';
+//import { ConfigureStore } from './redux/configureStore';
 
-import { Header } from './components/headerComponent.js'
+import Header from './components/headerComponent'
 
-const store = ConfigureStore();
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <div className="App">
-            <Header />
-          </div>
-        </BrowserRouter>
-      </Provider>
+        <div className="App">
+          <Header />
+        </div>
     );
   }
 }
