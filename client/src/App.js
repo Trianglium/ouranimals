35c7943fa7daeac6940ff4d35d58ae923dd3e510
Trianglium <jbrockerrudow@gmail.com>
2022-04-06 08:21:18 -0500

    Implement header and nav

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import { BrowserRouter } from 'react-router-dom';
 
-import Header from './components/headerComponent';
+import HeaderComponent from './components/headerComponent';
 
 
 class App extends Component {
   render() {
     return (
         <div className="App">
-          <Header />
+          <HeaderComponent />
           <h1>OurAnimals</h1>
         </div>
     );
--- client/src/components/headerComponent.js
+++ client/src/components/headerComponent.js
@@ -2,25 +2,56 @@ import React, { Component } from 'react';
 import { Media, UncontrolledCarousel, Nav, Navbar, NavbarBrand, Collapse, NavItem, Button } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
-function Header(props) {
-    return(
-      <div className="header">
-        <Navbar dark color="primary">
-        <div className="container">
-        <NavbarBrand className="mr-auto" href="/">OurAnimals</NavbarBrand>
-          <Nav className="ml-auto">
-            <NavItem>
-            <NavLink className="nav-link"  to='/bears'>Bears</NavLink>
-            <NavLink className="nav-link"  to='/dogs'>Dogs</NavLink>
-            <NavLink className="nav-link"  to='/foxes'>Foxes</NavLink>
-            <NavLink className="nav-link"  to='/otters'>Otters</NavLink>
-            <NavLink className="nav-link"  to='/docs'>Documentation</NavLink>
-            </NavItem>
-          </Nav>
-        </div>
-        </Navbar>
-      </div>
-    )
+
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggleNav = this.toggleNav.bind(this);
+        this.state = {
+          isNavOpen: false
+        };
+      }
+
+      toggleNav() {
+        this.setState({
+          isNavOpen: !this.state.isNavOpen
+        });
+      }
+
+    render() {
+        return(
+            <div>
+                <Navbar dark expand="md">
+                    <div className="container">
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className="mr-auto" href="/">OurAnimals</NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                            <NavItem>
+                            <NavLink className="nav-link"  to='/bears'>Bears</NavLink>
+                            <NavLink className="nav-link"  to='/dogs'>Dogs</NavLink>
+                            <NavLink className="nav-link"  to='/foxes'>Foxes</NavLink>
+                            <NavLink className="nav-link"  to='/otters'>Otters</NavLink>
+                            <NavLink className="nav-link"  to='/docs'>Documentation</NavLink>
+                            </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </div>
+                </Navbar>
+                <div className="jumbotron">
+                    <div className="container">
+                        <div className="row row-header">
+                            <div className="col-12 col-sm-6">
+                                <h1>OurAnimals</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Header;
