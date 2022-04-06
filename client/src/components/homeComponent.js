525198114012c1434f81441c5da72c8f50c5b0f4
Trianglium <jbrockerrudow@gmail.com>
2022-04-06 08:21:41 -0500

    Container home component for routing

--- client/src/components/homeComponent.js
+++ client/src/components/homeComponent.js
@@ -1,5 +1,33 @@
 import React, { Component } from 'react';
 import { Media } from 'reactstrap';
+import { Loading } from './loadingComponent';
+
+
+function RenderCard({item, isLoading, errMess}) {
+
+    if (isLoading) {
+        return(
+                <Loading />
+        );
+    }
+    else if (errMess) {
+        return(
+                <h4>{errMess}</h4>
+        );
+    }
+    else
+        return(
+            <Card>
+                <CardImg src={item.image} alt={item.name} />
+                <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+                <CardText>{item.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+
+}
 
 class Home extends Component {
   constructor(props) {
@@ -9,7 +37,10 @@ class Home extends Component {
     }
     return (
       <div className="container">
-        
+      </div>
+      <div>
+        <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess}  />
+
       </div>
     )
   }
