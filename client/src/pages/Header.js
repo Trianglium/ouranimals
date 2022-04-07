04e5736d21bef5f6fc216950845934013c849818
Trianglium <jbrockerrudow@gmail.com>
2022-04-07 12:02:09 -0500

    Header layout comp & All Animals page

---
+++ client/src/pages/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Welcome to OurAnimals</h1>
+        <p>The only image library that focuses on all of your favorite animals, and nothing more.</p>
+      </div>
+    );
+  }
+}
+
+export default Header
