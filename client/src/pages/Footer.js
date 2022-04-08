2f3aad52bc0f23feff1397bbbe7aae0db728662b
Trianglium <jbrockerrudow@gmail.com>
2022-04-08 05:06:37 -0500

    Footer layout component initialized

--- client/src/pages/Footer.js
+++ client/src/pages/Footer.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  List,
+  ListGroup
+} from 'reactstrap';
 
 class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        <h2></h2>
+        <Container>
+          <Row>
+            <Col xs={1} sm={1}>
+            </Col>
+            <Col xs={12} sm={3}>
+              <h4>Image Library</h4>
+              <ul>
+              <li><a href="/animals">Browse All Animals</a></li>
+              <li><a href="/bears">Bears</a></li>
+              <li><a href="/dogs">Dogs</a></li>
+              <li><a href="/foxes">Foxes</a></li>
+              <li><a href="/otters">Otters</a></li>
+              </ul>
+            </Col>
+            <Col xs={12} sm={3}>
+              <a href="https://github.com/Trianglium/ouranimals">GitHub</a></Col>
+            <Col xs={12} sm={3}>
+              <a href="/docs">GitHub</a>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
