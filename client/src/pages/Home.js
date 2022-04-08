fe0f9deac288fd46e30a29cfa905fb5c3b526f8e
Trianglium <jbrockerrudow@gmail.com>
2022-04-08 13:32:10 -0500

    Container comp Home, Header comp integrated

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Card, CardTitle, CardText, CardImg, Button, UncontrolledCarousel } from 'reactstrap';
+import Header from './Header';
 
 class Home extends Component {
   render() {
     return (
       <Container>
+        <Row>
+          <Header />
+        </Row>
         <Row>
           <Col xs={12} sm={6}>
 
