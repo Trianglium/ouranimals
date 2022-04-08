2f2e1a9a0d6f1d782dbfac9bb11d50bea176776c
Trianglium <jbrockerrudow@gmail.com>
2022-04-08 05:08:44 -0500

    Update global styles for layout components and theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
+import { Container } from 'reactstrap';
 import './App.css';
-
 import Header from './pages/Header';
 import Navigation from './pages/Navbar';
 import Home from './pages/Home';
+import ImageLibrary from './pages/ImageLibrary';
 import Footer from './pages/Footer';
-
 
 function App() {
     return (
-      <div>
-        <Header />
-        <Navigation />
-        <div className="row">
-          <Home />
-        </div>
-        <Footer />
+      <div >
+        <Container>
+          <Header />
+          <Navigation />
+          <div className="row">
+            <ImageLibrary />
+          </div>
+          <Footer />
+        </Container>
       </div>
     );
 }
