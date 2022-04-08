89d3eba961a1924cfad9449b69ec7998edcd09a6
Trianglium <jbrockerrudow@gmail.com>
2022-04-08 05:48:23 -0500

    Initial ImageDetail container component & mock promos

---
+++ client/src/pages/ImageDetail.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  List,
+  ListGroup
+} from 'reactstrap';
+
+
+class ImageDetail extends Component {
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col xs={12} sm={6} md={3}>
+            </Col>
+            <Col xs={12} sm={6} md={3}>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default ImageDetail
