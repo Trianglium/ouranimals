2e8f29e7c03822fadff17f00c29b8b35c7760b94
Jordan <jbrockerrudow@gmail.com>
2022-06-18 11:15:13 -0500

    Refine model fields & add search function

--- model/animal.js
+++ model/animal.js
@@ -2,15 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const AnimalImageSchema = new Schema ({
-    animal: {
+    type: {
         type:String,
-        required:[true, 'Animal Species Required']
+        required:[true, 'Animal Type Required']
     },
     image: {
         type:String,
         required:[true, 'Image Path Required']
     }
 });
+AnimalImageSchema.methods.findSimilarType = function findSimilarType (cb) {
+    return this.model('Animal').find({ type: this.type }, cb);
+};
 
 const Animal = mongoose.model('animal', AnimalImageSchema, "animal");
 
