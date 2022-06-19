175c75d76c6b4a8cabb47bb09b5ac1421f027313
Jordan <jbrockerrudow@gmail.com>
2022-06-19 09:50:56 -0500

    update OurAnimals component to interact with server

--- component/OurAnimals.js
+++ component/OurAnimals.js
@@ -5,26 +5,50 @@ class OurAnimals extends Component {
         super(props);
         this.state = {
             error: null,
-            isLoaded: false
+            isLoaded: false,
+            animals: []
         };
     }
 
     componentDidMount() {
-        fetch()
+        fetch('http://localhost:8081/api/image')
             .then(res => res.json())
             .then(
                 (data) => {
                     this.setState({
                         isLoaded: true,
+                        animals: data
                     });
                 },
                 (error) => {
                     this.setState({
                         isLoaded: true,
+                        error
                     });
                 }
             )
     }
-}
+
+
+    render() {
+        const { error, isLoaded, animals } = this.state;
+        if (error) {
+            return (
+                <div>Error: {error.message}</div>
+            );
+        } else if (!isLoaded) {
+            return (
+            <div>Loading...</div>
+            );
+        } else {
+            return (
+                <div>
+                  {animals.map((animal_im) => ((<img src={animal_im.image} alt={animal_im.type}>)))}
+                </div>
+            );
+        }
+
+    }
+};
 
 export default OurAnimals;
