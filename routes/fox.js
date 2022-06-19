eb2b2eb326627b835988b0ec47d3e2ae2037c61a
Jordan <jbrockerrudow@gmail.com>
2022-06-19 09:12:14 -0500

    Implement Fetch Fox Type

--- routes/fox.js
+++ routes/fox.js
@@ -4,8 +4,16 @@ const Animal = require('../model/animal');
 
 // http://localhost:27017/api/image/fox
 foxRouter.get('/image/fox', function (req, res, next) {
-  
-  // https://mongoosejs.com/docs/2.7.x/docs/methods-statics.html
+    Animal
+        .find({
+        type: 'fox'   // search query
+        })
+        .then(doc => {
+        console.log(doc)
+        })
+        .catch(err => {
+        console.error(err)
+        })
 });
 
 
