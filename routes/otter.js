b4ae33037c876e56f1fd8348b04bc1284d5f5258
Jordan <jbrockerrudow@gmail.com>
2022-06-19 09:11:52 -0500

    Implement Fetch Otter Type

--- routes/otter.js
+++ routes/otter.js
@@ -4,8 +4,16 @@ const Animal = require('../model/animal');
 
 // http://localhost:27017/api/image/otter
 otterRouter.get('/image/otter', function (req, res, next) {
-  
-  // https://mongoosejs.com/docs/2.7.x/docs/methods-statics.html
+    Animal
+        .find({
+        type: 'otter'   // search query
+        })
+        .then(doc => {
+        console.log(doc)
+        })
+        .catch(err => {
+        console.error(err)
+        });
 });
 
 
