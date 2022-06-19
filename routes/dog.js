def7820d2c0557ca858d58e3f85892bcf0918b62
Jordan <jbrockerrudow@gmail.com>
2022-06-19 09:12:07 -0500

    Implement Fetch Dog Type

--- routes/dog.js
+++ routes/dog.js
@@ -4,8 +4,16 @@ const Animal = require('../model/animal');
 
 // http://localhost:27017/api/image/dog
 dogRouter.get('/image/dog', function (req, res, next) {
-  
-  // https://mongoosejs.com/docs/2.7.x/docs/methods-statics.html
+    Animal
+        .find({
+        type: 'dog'   // search query
+        })
+        .then(doc => {
+        console.log(doc)
+        })
+        .catch(err => {
+        console.error(err)
+        })
 });
 
 
