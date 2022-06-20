e53c45ccdbe93082abccf1270316bb9ff455ae94
Jordan <jbrockerrudow@gmail.com>
2022-06-20 13:02:49 -0500

    Update route path

--- routes/images.js
+++ routes/images.js
@@ -2,8 +2,8 @@ const express = require('express');
 const animalImageRouter = express.Router();
 const Animal = require('../model/animal');
 
-// http://localhost:27017/api/image
-animalImageRouter.get('/image', function (req, res, next) {
+// http://localhost:27017/api/images
+animalImageRouter.get('/images', function (req, res, next) {
   Animal.aggregate([{
     $sample: { size: 1 }
   }]).then(function (animal) {
